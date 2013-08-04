daf69890b140239e49e5d754c23790419458959f
Damien DeVille <d.deville@me.com>
2013-08-04 12:02:35 +0100

    Use the new bookmark data API.

--- Framework/CDClassDumpOperation.m
+++ Framework/CDClassDumpOperation.m
@@ -95,10 +95,10 @@
 	[self setConnection:connection];
 	
 	NSXPCInterface *classDumpServerInterface = [NSXPCInterface interfaceWithProtocol:@protocol(CDClassDumpServerInterface)];
-	[classDumpServerInterface setClasses:[NSSet setWithObjects:[NSURL class], nil] forSelector:@selector(classDumpBundleOrExecutableAtLocation:exportDirectoryLocation:response:) argumentIndex:0 ofReply:NO];
-	[classDumpServerInterface setClasses:[NSSet setWithObjects:[NSURL class], nil] forSelector:@selector(classDumpBundleOrExecutableAtLocation:exportDirectoryLocation:response:) argumentIndex:1 ofReply:NO];
-	[classDumpServerInterface setClasses:[NSSet setWithObjects:[NSURL class], nil] forSelector:@selector(classDumpBundleOrExecutableAtLocation:exportDirectoryLocation:response:) argumentIndex:0 ofReply:YES];
-	[classDumpServerInterface setClasses:[NSSet setWithObjects:[NSError class], nil] forSelector:@selector(classDumpBundleOrExecutableAtLocation:exportDirectoryLocation:response:) argumentIndex:1 ofReply:YES];
+	[classDumpServerInterface setClasses:[NSSet setWithObjects:[NSURL class], nil] forSelector:@selector(classDumpBundleOrExecutableAtLocation:exportDirectoryBookmarkData:response:) argumentIndex:0 ofReply:NO];
+	[classDumpServerInterface setClasses:[NSSet setWithObjects:[NSData class], nil] forSelector:@selector(classDumpBundleOrExecutableAtLocation:exportDirectoryBookmarkData:response:) argumentIndex:1 ofReply:NO];
+	[classDumpServerInterface setClasses:[NSSet setWithObjects:[NSURL class], nil] forSelector:@selector(classDumpBundleOrExecutableAtLocation:exportDirectoryBookmarkData:response:) argumentIndex:0 ofReply:YES];
+	[classDumpServerInterface setClasses:[NSSet setWithObjects:[NSError class], nil] forSelector:@selector(classDumpBundleOrExecutableAtLocation:exportDirectoryBookmarkData:response:) argumentIndex:1 ofReply:YES];
 	
 	[connection setRemoteObjectInterface:classDumpServerInterface];
 	[connection resume];
@@ -106,6 +106,18 @@
 
 - (void)_doAsynchronousWorkWithReacquirer:(void (^)(void))reacquirer
 {
+	NSError *bookmarkDataCreationError = nil;
+	NSData *exportDirectoryBookmarkData = [self _createExportDirectoryBookmarkData:&bookmarkDataCreationError];
+	if (exportDirectoryBookmarkData == nil) {
+		[self setCompletionProvider:^ NSURL * (NSError **errorRef) {
+			if (errorRef != NULL) {
+				*errorRef = bookmarkDataCreationError;
+			}
+			return nil;
+		}];
+		return;
+	}
+	
 	id <CDClassDumpServerInterface> classDumpServer = [[self connection] remoteObjectProxyWithErrorHandler:^ (NSError *error) {
 		NSError *classDumpError = [self _remoteProxyObjectError:error];
 		[self setCompletionProvider:^ id (NSError **errorRef) {
@@ -118,7 +130,7 @@
 		reacquirer();
 	}];
 	
-	[classDumpServer classDumpBundleOrExecutableAtLocation:[self bundleOrExecutableLocation] exportDirectoryLocation:[self exportDirectoryLocation] response:^ (NSURL *exportDirectoryLocation, NSError *error) {
+	[classDumpServer classDumpBundleOrExecutableAtLocation:[self bundleOrExecutableLocation] exportDirectoryBookmarkData:exportDirectoryBookmarkData response:^ (NSURL *exportDirectoryLocation, NSError *error) {
 		[self setCompletionProvider:^ NSURL * (NSError **errorRef) {
 			if (errorRef != NULL) {
 				*errorRef = error;
@@ -128,6 +140,38 @@
 		
 		reacquirer();
 	}];
+}
+
+- (NSData *)_createExportDirectoryBookmarkData:(NSError **)errorRef
+{
+	void (^wrapAndPopulateError)(NSError *) = ^ void (NSError *error) {
+		if (errorRef != NULL) {
+			NSDictionary *userInfo = @{
+				NSLocalizedDescriptionKey : NSLocalizedStringFromTableInBundle(@"Couldn\u2019t create the export directory", nil, [NSBundle bundleWithIdentifier:CDClassDumpServiceBundleIdentifier], @"_CDClassDumpOperation export directory creation error description"),
+				NSLocalizedRecoverySuggestionErrorKey : NSLocalizedStringFromTableInBundle(@"There was an unknown error while creating the export directory. Please try again.", nil, [NSBundle bundleWithIdentifier:CDClassDumpServiceBundleIdentifier], @"_CDClassDumpOperation export directory creation error recovery suggestion"),
+				NSUnderlyingErrorKey : error,
+			};
+			*errorRef = [NSError errorWithDomain:CDClassDumpErrorDomain code:CDClassDumpErrorExportDirectoryCreationError userInfo:userInfo];
+		}
+	};
+	
+	NSURL *exportLocation = [self exportDirectoryLocation];
+	
+	NSError *exportDirectoryCreationError = nil;
+	BOOL exportDirectoryCreated = [[NSFileManager defaultManager] createDirectoryAtURL:exportLocation withIntermediateDirectories:YES attributes:nil error:&exportDirectoryCreationError];
+	if (!exportDirectoryCreated) {
+		wrapAndPopulateError(exportDirectoryCreationError);
+		return nil;
+	}
+	
+	NSError *bookmarkDataCreationError = nil;
+	NSData *bookmarkData = [exportLocation bookmarkDataWithOptions:(NSURLBookmarkCreationOptions)0 includingResourceValuesForKeys:nil relativeToURL:nil error:&bookmarkDataCreationError];
+	if (bookmarkData == nil) {
+		wrapAndPopulateError(bookmarkDataCreationError);
+		return nil;
+	}
+	
+	return bookmarkData;
 }
 
 - (NSError *)_remoteProxyObjectError:(NSError *)error
