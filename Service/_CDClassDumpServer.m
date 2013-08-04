b6bda86d460eec8c47a31f6e1a616d0e536e0d36
Damien DeVille <d.deville@me.com>
2013-08-04 12:02:43 +0100

    Use the new bookmark data API.

--- Service/_CDClassDumpServer.m
+++ Service/_CDClassDumpServer.m
@@ -12,6 +12,9 @@
 
 #import "_CDClassDumpInternalOperation.h"
 
+#import "ClassDump-Constants.h"
+#import "ClassDumpService-Constants.h"
+
 @interface _CDClassDumpServer ()
 
 @property (strong, nonatomic) NSOperationQueue *operationQueue;
@@ -34,8 +37,15 @@
 
 #pragma mark - CDClassDumpServerInterface
 
-- (void)classDumpBundleOrExecutableAtLocation:(NSURL *)bundleOrExecutableLocation exportDirectoryLocation:(NSURL *)exportDirectoryLocation response:(void (^)(NSURL *exportDirectoryLocation, NSError *error))response
+- (void)classDumpBundleOrExecutableAtLocation:(NSURL *)bundleOrExecutableLocation exportDirectoryBookmarkData:(NSData *)exportDirectoryBookmarkData response:(void (^)(NSURL *exportDirectoryLocation, NSError *error))response
 {
+	NSError *exportLocationRetrievalError = nil;
+	NSURL *exportDirectoryLocation = [self _retrieveExportDirectoryLocation:exportDirectoryBookmarkData error:&exportLocationRetrievalError];
+	if (exportDirectoryLocation == nil) {
+		response(nil, exportLocationRetrievalError);
+		return;
+	}
+	
 	_CDClassDumpInternalOperation *classDumpOperation = [[_CDClassDumpInternalOperation alloc] initWithBundleOrExecutableLocation:bundleOrExecutableLocation exportDirectoryLocation:exportDirectoryLocation];
 	[[self operationQueue] addOperation:classDumpOperation];
 	
@@ -46,6 +56,27 @@
 	}];
 	[responseOperation addDependency:classDumpOperation];
 	[[self operationQueue] addOperation:responseOperation];
+}
+
+#pragma mark - Private
+
+- (NSURL *)_retrieveExportDirectoryLocation:(NSData *)exportDirectoryBookmarkData error:(NSError **)errorRef
+{
+	NSError *exportLocationRetrievalError = nil;
+	NSURL *exportDirectoryLocation = [NSURL URLByResolvingBookmarkData:exportDirectoryBookmarkData options:(NSURLBookmarkResolutionOptions)0 relativeToURL:nil bookmarkDataIsStale:NULL error:&exportLocationRetrievalError];
+	if (exportDirectoryLocation == nil) {
+		if (errorRef != NULL) {
+			NSDictionary *userInfo = @{
+				NSLocalizedDescriptionKey : NSLocalizedStringFromTableInBundle(@"Couldn\u2019t create the export directory", nil, [NSBundle bundleWithIdentifier:CDClassDumpServiceBundleIdentifier], @"_CDClassDumpServer export directory creation error description"),
+				NSLocalizedRecoverySuggestionErrorKey : NSLocalizedStringFromTableInBundle(@"There was an unknown error while creating the export directory. Please try again.", nil, [NSBundle bundleWithIdentifier:CDClassDumpServiceBundleIdentifier], @"_CDClassDumpServer export directory creation error recovery suggestion"),
+				NSUnderlyingErrorKey : exportLocationRetrievalError,
+			};
+			*errorRef = [NSError errorWithDomain:CDClassDumpErrorDomain code:CDClassDumpErrorExportDirectoryCreationError userInfo:userInfo];
+		}
+		return nil;
+	}
+	
+	return exportDirectoryLocation;
 }
 
 #pragma mark - NSXPCListenerDelegate
